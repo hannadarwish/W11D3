7bed35304f5ac10a7079ba09ef5b35fc387685f9
Kenny Ngo <kennyvungo@gmail.com>
2023-06-07 14:43:02 -0700

    finished phase 6 and bonuses

--- practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Hygrometer/Hygrometer.js
+++ practice-for-week-14-react-context-greenhouse-long-practice-main/src/components/Hygrometer/Hygrometer.js
@@ -2,16 +2,31 @@ import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 import { ClimateContext } from "../../context/ClimateContext";
 import { useClimate } from "../../context/ClimateContext";
+import { useState,useEffect } from "react";
 
 function Hygrometer() {
   const {humidity,setHumidity} = useClimate();
+  const [desiredHumidity,setDesiredHumidity] = useState(humidity);
+
+  useEffect(() => {
+    const changeHumidity = setTimeout( () => {
+      if (desiredHumidity > humidity){
+        setHumidity(humidity + 2)
+      }else if (desiredHumidity < humidity){
+        setHumidity(humidity - 2)
+      }
+    },1000)
+    if(desiredHumidity === humidity){
+      return clearTimeout(changeHumidity);
+    }
+  },[humidity,desiredHumidity])
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={humidity}
-        onAfterChange={(val) => {setHumidity(val)}}
+        value={desiredHumidity}
+        onAfterChange={(val) => {setDesiredHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
